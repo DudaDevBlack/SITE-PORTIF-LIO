25fac4e640f181c71a187b7159ce4f1114f95d9c
Eduardo José Ferreira da Silva <101364719+DudaDevBlack@users.noreply.github.com>
2026-04-21 10:59:58 -0300

    Update script.js 21/04

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
-function navigate(sectionId) {
-    // Remove 'active' de todas as seções e itens do menu
-    document.querySelectorAll('.section').forEach(section => section.classList.remove('active'));
-    document.querySelectorAll('.menu-item').forEach(item => item.classList.remove('active'));
+const ESP32 = "http://192.168.0.100"; // MUDE
 
-    // Adiciona 'active' à seção e ao item de menu clicado
-    document.getElementById(sectionId).classList.add('active');
-    document.querySelector(`.menu-item[onclick="navigate('${sectionId}')"]`).classList.add('active');
+let grafico;
+
+async function atualizar(){
+
+  const r = await fetch(`${ESP32}/status`);
+  const d = await r.json();
+
+  document.getElementById("temp").innerText = d.temp + "°C";
+  document.getElementById("umid").innerText = d.umid + "%";
+  document.getElementById("tempoOnline").innerText = d.tempo;
+
+  let perc = Math.min((d.nivel / 200) * 100, 100);
+
+  document.getElementById("nivelBar").style.width = perc + "%";
+  document.getElementById("nivelTxt").innerText = d.nivel + " cm";
+
+  luzInt.checked = d.luzInt;
+  luzExt.checked = d.luzExt;
+  refletor.checked = d.refletor;
+  piscina.checked = d.piscina;
+
+  atualizarGrafico(d.temp);
 }
+
+function enviar(cmd){
+  fetch(`${ESP32}/${cmd}`);
+}
+
+luzInt.onchange = ()=>enviar("int");
+luzExt.onchange = ()=>enviar("ext");
+refletor.onchange = ()=>enviar("ref");
+piscina.onchange = ()=>enviar("pis");
+
+function iniciarGrafico(){
+  const ctx = document.getElementById("grafico");
+
+  grafico = new Chart(ctx,{
+    type:"line",
+    data:{
+      labels:[],
+      datasets:[{
+        label:"Temperatura",
+        data:[]
+      }]
+    }
+  });
+}
+
+function atualizarGrafico(temp){
+  let t = new Date().toLocaleTimeString();
+
+  grafico.data.labels.push(t);
+  grafico.data.datasets[0].data.push(temp);
+
+  if(grafico.data.labels.length > 10){
+    grafico.data.labels.shift();
+    grafico.data.datasets[0].data.shift();
+  }
+
+  grafico.update();
+}
+
+setInterval(atualizar,2000);
+iniciarGrafico();
